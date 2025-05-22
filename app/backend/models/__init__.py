957dcb2cd5ca78cd2838c57081b3208f7571557c
Miguel Righi <148013857+mrighi9@users.noreply.github.com>
2025-05-22 17:02:56 -0300

    backendv4

--- app/backend/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# backend/app/models/__init__.py
-from app.database import Base               # expõe Base (declarative_base)
-
-# importe todos os modelos individuais aqui
-from .user import User
-
-__all__ = ["Base", "User"]
